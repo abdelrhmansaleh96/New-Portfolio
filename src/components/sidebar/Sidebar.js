b2268f15c4290cc122d7f10570747f1b47235072
abdelrhman saleh <81262213+abdelrhmansaleh96@users.noreply.github.com>
2022-09-25 16:21:53 +0200

    finished

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./Sidebar.css";
 import { HashLink as Link } from "react-router-hash-link";
-import { FiExternalLink } from "react-icons/fi";
 import logo from "../images/sam.jpg";
 
 class Sidebar extends Component {
